2bf1ae01aa770bff07e526a2a692ae544b52db3f
Thomas <163157813+Thomasbka@users.noreply.github.com>
2024-09-25 10:53:38 +0200

    Created a computerPlay function.

---
+++ script.js
@@ -0,0 +1,6 @@
+const computerPlay = function() {
+  const choices = ["Rock", "Paper", "Scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+};
+
