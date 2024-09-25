1dda12ae57fe122337ade39b3199626fe97e3b61
Manobrawn <161127661+Manobrawn@users.noreply.github.com>
2024-09-26 00:06:00 +0200

    just added alert instructions

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+alert(`
+Welcome to Rock, Paper or Scissors game!
+Here's how to play:
+1. Make your choice: Rock, Paper or Scissors.
+2. You will be playing against the computer 5 rounds. Whoever wins 3 rounds first wins! 
+3. Your input should be given in the Prompt window.
+4. To play, please use the console on your browser's inspection tool. 
+Good luck! :)
+`)
+
 const computerPlay = function() {
   const choices = ["Rock", "Paper", "Scissors"];
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
 };
 
-const playRound = function(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
   const playerChoice = playerSelection.toLowerCase();
   const computerChoice = computerSelection.toLowerCase();
 
