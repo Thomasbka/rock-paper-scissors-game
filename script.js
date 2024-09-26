d9335031aa23ac1d1adb1101e0fc80bf662ca7e3
Thomas <163157813+Thomasbka@users.noreply.github.com>
2024-09-26 10:19:04 +0200

    small adjustments to the game. Added a playAgain function, asking the player if they want to play again. Also added a setTimeouet in order for the console to load it all. I also added a playerResult function that displays the result as an alert.

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ Welcome to the Rock, Paper, Scissors game!
 
 - Here you get to play against your own computer in a match best of 5.
 - In order to play this game you will need to open your inspecter window and use the console tab.
-- After clicking 'OK' you can type your chose inside the box.
+- After clicking 'OK' you can type your choice inside the box.
 - You will afterwards see the result in the console.
 
-May the best player win!`);
+May the best player win!
+`);
 
 const computerPlay = function() {
   const choices = ["Rock", "Paper", "Scissors"];
@@ -32,6 +33,8 @@ const playRound = function(playerSelection, computerSelection) {
 };
 
 const game = function() {
+  console.clear();
+  
   let playerScore = 0;
   let computerScore = 0;
   const validChoices = ["rock", "paper", "scissors"];
@@ -42,7 +45,7 @@ const game = function() {
     while (!validChoices.includes(playerSelection)) {
       playerSelection = prompt("Your choice is invalid. Please choose either Rock, Paper or Scissors:").toLowerCase();
     }
-    
+
     const computerSelection = computerPlay();
     const result = playRound(playerSelection, computerSelection);
 
@@ -54,16 +57,40 @@ const game = function() {
       computerScore++;
     }
 
-    console.log(`Round ${i+1}: Player - ${playerScore}, Computer - ${computerScore}`);
+    console.log(`Round ${i + 1}: Player - ${playerScore}, Computer - ${computerScore}`);
+
+    if (i === 3) {
+      console.log(`LAST ROUND! Let's see if you can beat the computer!`);
+    }
   }
 
+  setTimeout(function() {
+    playerResult(playerScore, computerScore);
+  }, 100);
+};
+
+const playerResult = function(playerScore, computerScore) {
   if (playerScore > computerScore) {
-    console.log("You won!");
-  } else if ( computerScore > playerScore) {
-    console.log("You Lose!");
+    alert("Congratulations, you won the game!");
+    console.log("You won the game!");
+  } else if (computerScore > playerScore) {
+    alert("Sorry, you lost the game.");
+    console.log("You lost the game.");
   } else {
+    alert("It's a tie game!");
     console.log("The game is a tie!");
+  }
+
+  playAgain();
+};
+
+const playAgain = function() {
+  let response = prompt("Do you want to play again? (yes/no)").toLowerCase();
+  if (response === "yes") {
+    game();
+  } else {
+    alert("Thanks for playing! See you next time.");
   }
 };
 
-game();
+setTimeout(game, 100);
